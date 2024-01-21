0d1c6b078d612ca4f5a41c152698d49a2e1d3195
jay-juns <necrozerg2@naver.com>
2024-01-21 18:02:23 +0900

    Initial Commit

---
+++ ReadingBooks/ReadingBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingBooksApp.swift
+//  ReadingBooks
+//
+//  Created by junseok Kim on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct ReadingBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
